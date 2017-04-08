f3438bb7284ae55db5f1bc8d04e8b0e3b1cdf28a
German Caro <german.layer8@gmail.com>
2017-04-08 19:26:00 -0300

    Cambio time por urandom como semilla

--- percolacion.c
+++ percolacion.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include "hoshen.h"
 #include "auxiliares.h"
+#include "ns.h"
 
 #define P     16             // 1/2^P, P=16
 #define Z     27000          // iteraciones
@@ -14,36 +15,40 @@ void  llenar(int *red,int n,float prob);
 
 int main(int argc,char *argv[])
 {
-  int    *red;
-  float  prob;
+    int    *red;
+    float  prob;
+    FILE *urandom;
+    int seed;
 
-  int n=N;
-  int z=Z;
-// Le puedo dar como parametros el tamaño de la red y la precision 
-// en la probilidad
-  if (argc==3) 
+    int n=N;
+    int z=Z;
+    // Le puedo dar como parametros el tamaño de la red y la precision 
+    // en la probilidad
+    if (argc==3) 
      {
        sscanf(argv[1],"%d",&n);
        sscanf(argv[2],"%d",&z);
      }
-    
-  red=(int *)malloc(n*n*sizeof(int));
 
-  prob=0.5;
-  srand(time(NULL));
+    red=(int *)malloc(n*n*sizeof(int));
+
+    prob=0.5;
       
-  int i, j;
-  float denominador;
-  float suma = 0.0;
-  float sigma = 0.0;
-  float suma_cuadrado = 0.0;
+    int i, j;
+    float denominador;
+    float suma = 0.0;
+    float sigma = 0.0;
+    float suma_cuadrado = 0.0;
+    // Usar time hace que haga cientos de ciclos con la misma semilla
+    urandom = fopen("/dev/urandom", "r");
 // 27000 realizaciones para calcular la probabilidad critica
     for(i=0;i<z;i++)
     {
       prob=0.5;
       denominador=2.0;
 
-      srand(time(NULL));
+      fread(&seed, sizeof(seed), 1, urandom);
+      srand(seed);
     // Dentro de este bucle busco pc
       for(j=0;j<P;j++)
         {
@@ -60,13 +65,18 @@ int main(int argc,char *argv[])
           }
         }
       // Sumo para calcular despues el valor medio y el sigma
+        ns(red, n);
       suma = suma + prob/z;
       suma_cuadrado = suma_cuadrado + prob*prob/z;
     }
+    // Ahora calculo ns(s). En realidad calculo el numero de elementos por
+    // clusters, despues en python analizo esos datos
+
 
     sigma = sqrt(suma_cuadrado - suma*suma);
-  printf("%f %f", suma, sigma);
-  free(red);
+    printf("%f %f", suma, sigma);
+    free(red);
+    fclose(urandom);
 
   return 0;
 }
