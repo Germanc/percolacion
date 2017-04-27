b332f944c3f23e1720581d985591d5e3df42f39e
German Caro <german.layer8@gmail.com>
2017-04-27 18:01:20 -0300

    Tengo en cuenta los clusters percolantes
    Ahora si calculo ns(p) y m2(p) haciendo la suma para todo menos para los
    clusters percolantes

--- percolacion_m2.c
+++ percolacion_m2.c
@@ -10,15 +10,48 @@
 
 
 void  llenar(int *red,int n,float prob);
-void numero_s(int *red, int n, float *ns)
+void numero_s(int *red, int n, float *ns, float *ns_sinpercolante)
 {
     int *lista;
-    int i;
+    int *lista_sp;
+    int i,j,k;
+    int esta_repetido;
     lista = (int *)malloc(n*n*sizeof(int));
+    lista_sp = (int *)malloc(n*n*sizeof(int));
     for(i=0;i<n*n;i++) lista[i] = 0; 
-    for(i=0;i<n*n;i++) if(red[i]!=0) lista [red[i]] += 1;
+    for(i=0;i<n*n;i++) if(red[i]!=0) lista[red[i]] += 1;
     for(i=0;i<n*n;i++) if(lista[i]!=0) ns[lista[i]] += 1;
     free(lista);
+
+    int *lista_percolante;
+    int contador = 0;
+    lista_percolante = (int *)malloc(sizeof(int)*2*n);
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(red[i] == red[n*(n-1)+j]){
+                esta_repetido = 0;
+                for(k=0;k<contador;k++) if(lista_percolante[k] == red[i]) esta_repetido = 1;
+                if(esta_repetido == 0) {
+                    lista_percolante[contador] = red[i];
+                    contador +=1;
+                }
+            }
+        }
+    }
+    esta_repetido = 0;
+    for(i=0;i<n*n;i++) lista_sp[i] = 0; 
+    for(i=0;i<n*n;i++){ 
+        if(red[i]!=0) {
+            esta_repetido = 0;
+            for(j=0;j<2*n;j++){
+                if(red[i] == lista_percolante[j]) esta_repetido = 1;
+            }   
+            if(esta_repetido == 0) lista_sp[red[i]] += 1;
+        }
+    }
+    for(i=0;i<n*n;i++) if(lista_sp[i]!=0) ns_sinpercolante[lista_sp[i]] += 1;
+    free(lista_sp);
+
 }
 
 int main(int argc,char *argv[])
@@ -51,7 +84,9 @@ int main(int argc,char *argv[])
     // 27000 realizaciones para calcular la probabilidad critica
 
     float *ns;
+    float *ns_sinpercolante;
     ns = (float *)malloc(sizeof(float)*n*n);
+    ns_sinpercolante = (float *)malloc(sizeof(float)*n*n);
     int P = 80;
     float *m2;
     m2 = (float *)malloc(sizeof(float)*P);
@@ -66,11 +101,12 @@ int main(int argc,char *argv[])
             srand(seed);
             llenar(red, n, prob);
             hoshen(red, n);
-            numero_s(red,n,ns);
+            numero_s(red,n,ns,ns_sinpercolante);
             for(k=0;k<(n*n)/2;k++)
             {
                 ns[k] /= (z*n*n); // Normalizo el ns
-                m2[j] += ns[k]*k*k; // Calculo el m2(p)
+                ns_sinpercolante[k] /= (z*n*n); // Normalizo el ns
+                m2[j] += ns_sinpercolante[k]*k*k; // Calculo el m2(p)
             }
         
         }
